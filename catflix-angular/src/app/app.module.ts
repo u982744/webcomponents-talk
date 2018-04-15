8cf597df728355d2bea5c8d12e4b57f7be69b7b4
Tim Berman <timmpberman@192-168-1-102.tpgi.com.au>
2018-04-15 23:15:38 +1000

    react all working now

--- catflix-angular/src/app/app.module.ts
+++ catflix-angular/src/app/app.module.ts
@@ -1,13 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-// import '../webcomponents/catflix-button/catflix-button.html';
-// import '../webcomponents/catflix-cta/catflix-cta.html';
-// import '../webcomponents/catflix-logo/catflix-logo.html';
-
-// import '../../../webcomponents/catflix-button/catflix-button.html';
-// import '../webcomponents/catflix-profile-thumb/catflix-profile-thumb.html';
-
 import { AppComponent } from './app.component';
 
 
--- catflix-react/src/App.js
+++ catflix-react/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container flex">
+        <div className="logo">
+          <catflix-logo></catflix-logo>
+        </div>
+        <div className="cta">
+          <catflix-cta text="Who's watching?"></catflix-cta>
+        </div>
+        <div className="profiles">
+          <catflix-profile-thumb img="/assets/imgs/cat-2.jpg" name="Cee cee"></catflix-profile-thumb>
+          <catflix-profile-thumb img="/assets/imgs/cat-3.jpg" name="Cupcake"></catflix-profile-thumb>
+          <catflix-profile-thumb img="/assets/imgs/cat-6.jpg" name="Tweet tweet"></catflix-profile-thumb>
+          <catflix-profile-thumb img="/assets/imgs/cat-7.png" name="Garfield"></catflix-profile-thumb>
+        </div>
+        <div className="manage">
+          <catflix-button>Manage profiles</catflix-button>
+        </div>
       </div>
     );
   }
